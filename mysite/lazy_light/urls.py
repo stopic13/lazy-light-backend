9995cf063f7d9fa7b31d3ce2cb2a8001d92e9617
stopic13 <sara.topic.18@dartmouth.edu>
2019-01-12 22:33:44 -0600

    able to toggle

--- mysite/lazy_light/urls.py
+++ mysite/lazy_light/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'lazy_light'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('toggleLight', views.toggleLight, name='toggleLight'),
 ]
--- mysite/lazy_light/views.py
+++ mysite/lazy_light/views.py
@@ -1,16 +1,22 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404, redirect
 from django.template import loader
 
 from .models import Button
 
 
 def index(request):
-    light_on = Button.objects
+    button = get_object_or_404(Button)
     #[:1].get().light_on
+    light_on = button.light_on
     print("LIGHT_ON", light_on)
-    template = loader.get_template('lazy_light/index.html')
-    context = {
-        'light_on': light_on,
-    }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'lazy_light/index.html', {'light_on': light_on})
 
+def toggleLight(request):
+    # Get your button instance here
+    button = get_object_or_404(Button)
+    button.light_on = not button.light_on
+    button.save()
+   # light_on = get_object_or_404(Button).light_on
+
+    return redirect('lazy_light:index')
